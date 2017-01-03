82a50e76b3a91de971b81f013906d8936d43c8b7
garronej <joseph.garrone@ensimag.grenoble-inp.fr>
2017-01-03 09:37:03 +0000

    cool

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+
 /*
 interface ParsedAtCommand {
     commands: Object[];
@@ -11,25 +12,33 @@ interface parseAtCommand {
 
 
 declare module "at-commands-parser" {
-    export = fun;
-}
-
-declare var fun: parseAtCommand;
-*/
-export interface ParsedAtCommand {
-    commands: Object[];
-    raw: string;
-}
-
-
-export interface parseAtCommand {
-    (atCommand: string): ParsedAtCommand;
-}
-
-
-declare module "at-commands-parser" {
 
     var fun: parseAtCommand;
 
     export = fun;
 }
+*/
+
+
+
+
+
+declare module "at-commands-parser" {
+
+
+    export interface ParsedAtCommand {
+        commands: Object[];
+        raw: string;
+    }
+
+
+    export interface parseAtCommand {
+        (atCommand: string): ParsedAtCommand;
+    }
+
+    export function parser(atCommand: string): ParsedAtCommand;
+}
+
+
+
+
