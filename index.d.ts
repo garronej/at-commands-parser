2611ac464f13b3aa31da269673b59b04b490b3c0
garronej <joseph.garrone@ensimag.grenoble-inp.fr>
2017-01-03 09:09:33 +0000

    okokok

--- index.d.ts
+++ index.d.ts
@@ -16,19 +16,19 @@ declare module "at-commands-parser" {
 
 declare var fun: parseAtCommand;
 */
+interface ParsedAtCommand {
+    commands: Object[];
+    raw: string;
+}
+
+
+interface parseAtCommand {
+    (atCommand: string): ParsedAtCommand;
+}
 
 
 declare module "at-commands-parser" {
 
-    export interface ParsedAtCommand {
-        commands: Object[];
-        raw: string;
-    }
-
-
-    export interface parseAtCommand {
-        (atCommand: string): ParsedAtCommand;
-    }
 
     var fun: parseAtCommand;
 
